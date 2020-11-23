2075dd51373f804f8490edf6da1f28466cece74a
Haopeng Wang <Haopeng.Wang@fau.de>
2020-11-23 15:49:43 +0100

    final Range Sum

--- RangeSum.cpp
+++ RangeSum.cpp
@@ -4,17 +4,19 @@
 using namespace std;
 
 int main(){
+	while(1){
 	cout<<"Please input two numbers in a line and seperated by a blank(they could be integers and decimals) "<<endl;
 	float a,b;
 	scanf("%f %f",&a, &b);
 	int aa,bb;
 	aa = ceil(a);
 	bb = floor(b);
-	cout<<aa<<' '<<bb<<endl;
 	long long res = 0;
 	for(long long i = aa; i < bb; ++i)
 		res += i;
-	cout<< res<<endl;
-
+	cout<< res<<endl<<endl;
+	}
 	return 0;
 }
+
+
